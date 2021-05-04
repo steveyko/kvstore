6fe435e107c9714fe7e93903622c5e967fee5109
Steve Ko <steveyko@sfu.ca>
2021-05-03 17:12:17 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//! This is just an example.
+use kvstore::{KVStore, Operations};
+
+fn main() {
+    let mut kvs = KVStore::new(".").unwrap();
+    kvs.insert(String::from("key"), 2 as i32).unwrap();
+    kvs.lookup::<String, i32>(String::from("key")).unwrap();
+    kvs.remove::<String, i32>(String::from("key")).unwrap();
+}
